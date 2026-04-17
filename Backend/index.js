a731995b5aacda806fb425ebc5afd6572a12f48e
Jatin Tasoria <tasoriaj4@gmail.com>
2026-04-17 08:07:27 +0530

    Connect with Mongodb Atlas

--- Backend/index.js
+++ Backend/index.js
@@ -13,8 +13,8 @@ app.use(express.json());
 
 /* ================== MongoDB Connection ================== */
 mongoose.connect(process.env.MONGO_URI)
-  .then(() => console.log(" MongoDB connected"))
-  .catch((err) => console.error(" MongoDB error:", err));
+  .then(() => console.log("MongoDB connected"))
+  .catch(err => console.error("Mongo error:", err));
 
 /* ================== Models ================== */
 
