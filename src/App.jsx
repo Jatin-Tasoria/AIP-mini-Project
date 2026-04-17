de21a41af583788440569518bb8c0bc73cb04d91
Jatin Tasoria <tasoriaj4@gmail.com>
2026-04-17 10:07:14 +0530

    Given all temp names

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 
-import Front from '../src/Pages/Front'
+import Front from './Pages/Fronttemp.jsx'
 import './App.css'
 import Navbar from './components/Navbartemp/Navbar.jsx'
 import Footer from './components/Footertemp/Footer.jsx'
-import MenuPage from './Pages/Menu.jsx'
-import AboutPage from './Pages/About.jsx'
+import MenuPage from './Pages/Menutemp.jsx'
+import AboutPage from './Pages/Abouttemp.jsx'
 import AuthPage from './Pages/LoginTemp.jsx'
-import Cart from './Pages/Cart.jsx'
+import Cart from './Pages/Carttemp.jsx'
 import ReviewSection from './components/Commentstemp/Comments.jsx'
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import API from './api';
--- src/Pages/Fronttemp.jsx
+++ src/Pages/Fronttemp.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import Banner from '../components/Bannertemp/Banner.jsx'
 import PopularSection from '../components/PopularSectiontemp/PopularSection.jsx'
 import MenuButton from '../components/Buttontemp/Button.jsx'
-import API from '../api';
+import API from '../api.js';
 
 const Front = () => {
   return (
