63079b0013f16555aa01e2b305bba7374bf2be24
Jatin Tasoria <tasoriaj4@gmail.com>
2026-04-17 07:13:43 +0530

    Ready for deployment

--- Backend/index.js
+++ Backend/index.js
@@ -177,13 +177,4 @@ app.get("/api/orders", async (req, res) => {
 /* ================== Server ================== */
 
 const PORT = process.env.PORT || 5000;
-
-const path = require("path");
-
-app.use(express.static(path.join(__dirname, "dist")));
-
-app.use((req, res) => {
-  res.sendFile(path.join(__dirname, "dist", "index.html"));
-});
-
 app.listen(PORT, () => console.log(` Server running on port ${PORT}`));
